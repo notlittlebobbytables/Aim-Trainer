faee42b93198e2083fcb0a61485394b0309786b7
Bobby <notlittlebobbytables@gmail.com>
2020-07-13 20:07:25 -0500

    Changes to be committed:

--- aim.py
+++ aim.py
@@ -7,15 +7,21 @@ import time
 pygame.init()
 
 # Set Display
-surface = pygame.display.set_mode(size=(750, 500))
+surface = pygame.display.set_mode(size=(1920//2, 1080//2))
+
+text = pygame.font.SysFont("Comic Sans MS", 30)
+
+# Also Works
+# text = pygame.font.Font(None, 30)
 
 # Hide the mouse so its just a dot
 pygame.mouse.set_visible(False)
 
 test = 0
 timer = time.time()
-xpos = 0
-ypos = 0
+xpos = randrange(50, 700)
+ypos = randrange(50, 450)
+score = 0
 
 
 # Main Body Of Program
@@ -23,13 +29,20 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        # Checks to see if target was hit or not
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mouse[0] < xpos + 4 and mouse[0] > xpos - 4 and mouse[1] < ypos + 4 and mouse[1] > ypos - 4:
+                score += 1
+                timer = time.time()
+                xpos = randrange(50, 700)
+                ypos = randrange(50, 450)
+        
+    # Add else statement for when you miss??
     
     mouse = pygame.mouse.get_pos()
     surface.fill((0,0,0))
     pygame.draw.circle(surface, (255,255,255), mouse, 5)
-
-    text = pygame.font.Font(None, 20)
-    text.render("Hello", True, [200,200,0])
 
     # Display target
     if time.time() < timer + 2:
@@ -39,13 +52,7 @@ while True:
         xpos = randrange(50, 700)
         ypos = randrange(50, 450)
 
-    if mouse[0] < xpos + 2 and mouse[0] > xpos - 2 and mouse[1] < ypos + 2 and mouse[1] > ypos - 2:
-        test = test + 1
-        timer = time.time()
-        xpos = randrange(50, 700)
-        ypos = randrange(50, 450)
-        print(test)
-    # Add else statement for when you miss
+    surface.blit(text.render(f"Score: {score}", True, (255,255,255)), (0,0))
 
     # Update display
     pygame.display.update()
